a2b0d7916cda839281a8fb70852f2d157cefb1d0
RADI AHMED CHOWDHURY <chyradi.2000@gmail.com>
2025-01-16 19:27:18 +0600

    add project

---
+++ src/university/management/system/Conn.java
@@ -0,0 +1,28 @@
+
+package university.management.system;
+
+import java.sql.*;
+
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    Conn() {
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "radi511410");
+            s = c.createStatement();
+            
+            
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        
+        }
+    
+    }
+    
+}
